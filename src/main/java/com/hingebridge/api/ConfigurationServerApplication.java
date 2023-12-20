5a1d2d40eef5d47c61787cbf78ff3f86763d6c4e
brightchi <chinedu.ezeigbo@mtn.com>
2023-12-20 08:59:02 +0100

    Completed Config Server Prelim Impl

--- src/main/java/com/hingebridge/api/ConfigurationServerApplication.java
+++ src/main/java/com/hingebridge/api/ConfigurationServerApplication.java
@@ -2,12 +2,12 @@ package com.hingebridge.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigurationServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationServerApplication.class, args);
 	}
-
 }
